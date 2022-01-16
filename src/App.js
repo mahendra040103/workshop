bbc6c6640a154edf0e1f7a84f73bec36341c051b
mahendra040103 <97815819+mahendra040103@users.noreply.github.com>
2022-01-16 14:38:34 +0700

    first submit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import "./Assets/App.scss"
+
+function App() {
+
+
+
+
+  return (
+   <div className="container"> 
+   <div clashName ="navbar">header</div>
+   <div className="section" >
+     <div clashName = "card section-1">1</div>
+     <div clashName = "card section-2">2</div>
+     <div clashName = "card section-3">3</div>
+     <div clashName = "card section-4">4</div>
+     <div clashName = "card section-5">5</div>
+     <div clashName = "card section-6">6</div>     
+</div>
+</div>
+   );
+   }
+
+   export default App;
